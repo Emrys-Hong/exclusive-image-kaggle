022d54c3d15e26fda3ff4749f884655cf5031aa2
Hong Pengfei <35634481+Emrys-Hong@users.noreply.github.com>
2018-10-21 15:26:59 +0800

    Update purge.py

--- utils/purge.py
+++ utils/purge.py
@@ -1,5 +1,7 @@
 import os
+import glob
 
-file = '/path/to/file.mov'
-if os.path.getsize(file) < 100 * 1024:
-  os.remove(file)
+files = glob.glob("train90_size/*")
+for file in files:
+    if os.path.getsize(file) < 4.1 * 1024:
+        os.remove(file)
